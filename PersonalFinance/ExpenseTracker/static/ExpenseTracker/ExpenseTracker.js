b2d96e95d4f596e08b54139b0947376ff86e7929
Pieter <pieter.k@agiletechnica.com>
2023-03-15 09:08:04 +0700

    update 14 Feb 2023

--- PersonalFinance/ExpenseTracker/static/ExpenseTracker/ExpenseTracker.js
+++ PersonalFinance/ExpenseTracker/static/ExpenseTracker/ExpenseTracker.js
@@ -1,33 +1,35 @@
-$(function() {
-    const csrftoken = Cookies.get('csrftoken');
+$(function () {
+    
     // Default Page : home_view
+    var other_pages = $("div.page_view")
+    other_pages.hide();
     $("div#home_view").show()
 
     // Navigation Menu
-    $("button.nav_button").each(function(){
-        $(this).click(function(){
+    $("button.nav_button").each(function () {
+        $(this).click(function () {
             // Show clicked page, hide all other
             var other_pages = $("div.page_view")
             other_pages.hide();
 
-            var clicked_page = "div#"+$(this).attr("id")
+            var clicked_page = "div#" + $(this).attr("id")
             $(clicked_page).show()
 
-            // [TODO] AJAX Call based on page
-            if ($(this).attr("id")==="bankstatement_view") {
-                $("#bankstatement_input").change( function(){
+            // Bank Statement Page
+            if ($(this).attr("id") === "bankstatement_view") {
+
+                // feature: preview & check PDF
+                $("#bankstatement_input").change(function () {
                     var uploaded_file = $(this).prop('files')
                     if (uploaded_file[0].type !== "application/pdf") {
                         alert("Please Upload PDF File!")
                         location.reload()
                     } else {
-                        $('#pdf_preview').add("embed").attr("src", URL.createObjectURL(uploaded_file[0]) )
+                        $('#pdf_preview').add("embed").attr("src", URL.createObjectURL(uploaded_file[0]))
                     }
-                    console.log(URL.createObjectURL(uploaded_file[0]))
                 })
+                // [CONTINUE]: Bank Statement feature
             }
         });
     })
-
-    
 });
