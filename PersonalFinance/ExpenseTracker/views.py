81db88c7f36fb91505e3c51da059d22e7847b3d2
PieterCK <pieterceka123@gmail.com>
2023-03-19 22:00:15 +0700

    progress on bankstatement parsing | continue categorize transaction

--- PersonalFinance/ExpenseTracker/views.py
+++ PersonalFinance/ExpenseTracker/views.py
@@ -11,6 +11,7 @@ import json
 import PyPDF2
 import os
 import urllib
+import re
 from io import StringIO
 
 # Create your views here.
@@ -81,8 +82,20 @@ def bankstatement(request):
         handle_uploaded_file(request.FILES['file'])
         f = open('ExpenseTracker/static/ExpenseTracker/upload/'+request.FILES['file'].name, "rb")
         reader = PyPDF2.PdfReader(f)
-        page = reader.pages[3]
+        page = reader.pages[0]
         text = page.extract_text()
+        print(text)
+        text= re.split("(\d\d/\d\d)", text)
+        print(len(text))
+        i = 0
+        while i < len(text):
+            print(i)
+            if re.findall("TRANSAKSI DEBIT TGL:", text[i]):
+                print("Transaction at:",text[i+1],"debited :", text[i+2])
+                i += 4
+            else:
+                print("uncoded:",text[i])
+                i += 1
 
         CONTEXT["message"] = text
         return render(request, "ExpenseTracker/bankstatement.html",CONTEXT)
