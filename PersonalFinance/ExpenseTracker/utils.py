6b36e97a59cb505db13bf92d316dbf3cb15eddbc
PieterCK <pieterceka123@gmail.com>
2023-03-23 21:53:17 +0700

    progress on parsing bankstatement

--- PersonalFinance/ExpenseTracker/utils.py
+++ PersonalFinance/ExpenseTracker/utils.py
@@ -1,5 +1,11 @@
+import re
+
 def handle_uploaded_file(f):  
     with open('ExpenseTracker/static/ExpenseTracker/upload/'+f.name, 'wb+') as destination:  
         for chunk in f.chunks():  
             destination.write(chunk) 
 
+def split_string(source, separators):
+    separators = '|'.join(separators)
+    print(separators)
+    return re.split('('+separators+')', source)
