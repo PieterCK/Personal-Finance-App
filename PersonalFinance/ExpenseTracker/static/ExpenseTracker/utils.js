497973dbf8fe20e83757257581245e6752dc03ce
pieter ck <pieter.k@agiletechnica.com>
2023-06-02 17:21:50 +0700

    feat: progress on diagnose state in bankstatement view

--- PersonalFinance/ExpenseTracker/static/ExpenseTracker/utils.js
+++ PersonalFinance/ExpenseTracker/static/ExpenseTracker/utils.js
@@ -1,7 +1,15 @@
 function toggle_elements_view(elements, state){
-    elements.each(function(){
-        $(this).toggle(state)
-    })
+    console.log(elements)
+    if (Array.isArray(elements)){
+        elements.forEach(function(element){
+            console.log(element)
+            element.toggle(state)
+        })
+    }else{
+        elements.each(function(){
+            $(this).toggle(state)
+        })
+    }   
 }
 function display_pdf(query_selector , url){
     query_selector.add("embed").attr("src", url)
